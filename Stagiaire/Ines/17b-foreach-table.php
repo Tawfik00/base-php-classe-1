5855638b3796d3fa6fbeaf8154280d842425e89d
Ines2909 <ines.batik@cf2000.onmicrosoft.com>
2026-03-18 10:25:42 +0100

    exercice 17b : ajout d'un espace.

--- Stagiaire/Ines/17b-foreach-table.php
+++ Stagiaire/Ines/17b-foreach-table.php
@@ -1,6 +1,6 @@
 <?php
 
-$stagiaire = ["prenom" => "Inès", "nom" => "Batik", "email" =>"ines.batik@cf2000.onmicrosoft.com", "age" => 27];
+$stagiaire = ["prenom" => "Inès", "nom" => "Batik", "email" => "ines.batik@cf2000.onmicrosoft.com", "age" => 27];
 
 echo "<table border='1' style='border-collapse: collapse; width: 300px;'>";
 echo "<tr>
