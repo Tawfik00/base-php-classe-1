57389850a48c82bb983ab7fea95492793781fa64
soufianebouazza-hub <soufiane.bouazza1998@gmail.com>
2026-03-25 12:15:34 +0100

    controleurs frontal + mvc

--- Stagiaire/Soufiane/mvc-19/PUBLIQUE/index.php
+++ Stagiaire/Soufiane/mvc-19/PUBLIQUE/index.php
@@ -1,5 +1,24 @@
 <?php
+# Stagiaire/Michael/mvc-19/public/index.php
 
-require_once"../config.php"; 
+# ICI
+/* 
+Chargement une seule fois 
+et qui arrête 
+le script en cas d'échec 
+*/
+require_once "../config.php";
 
-echo ROOT_PATH;
+// si il n'existe pas de variable get nommée 'page'
+
+if(!isset($_GET['page'])){
+    include ROOT_PATH."/view/index.php" ;
+}elseif(in_array($_GET['page'],PUBLIC_PAGES)){
+
+   // si la variable get corespond a une valeurs 
+   // accpter dans le tableau
+   include ROOT_PATH."/view/".$_GET['page'].".php";
+}else{
+
+    include ROOT_PATH."/view/404.php";
+}
