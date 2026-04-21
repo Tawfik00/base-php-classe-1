be5c47bbf4a9a98cef98801b39b80771024a8b9c
Tao <crypto.tao@hotmail.com>
2026-04-21 11:14:43 +0200

    ex 5 et 6

---
+++ Stagiaire/Tawfik/06-variables-valides.php
@@ -0,0 +1,21 @@
+<?php 
+//string (chaine de caractère)
+$mot = "valeur de la variable";
+
+//integer (nombre entier)
+$nb = 123;
+
+//float (nombre décimal)
+$nbFloat = 123.23;
+
+//array (tableau indexé)
+$tab = [1, 2, 3];
+
+//array (tableau associatif)
+$personne = ["clef1" => "valeur1", "clef2" => "valeur2"];
+
+//null (abscence de valeur)
+$rien = null;
+
+//Affichage avec le débogeur var_dump()
+var_dump($mot, $nb, $nbFloat, $tab, $personne, $rien);
